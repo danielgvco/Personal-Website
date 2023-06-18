0388e25d16dff9af4c3830aca75eb3aece7ab3c5
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-18 15:57:00 -0500

    pages fix error for vercel

---
+++ app/projects/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Projects() {
+  return (
+    <h1>Projects</h1>
+  )
+}
---
+++ app/resume/page.tsx
@@ -0,0 +1,7 @@
+import Image from 'next/image'
+
+export default function Resume() {
+  return (
+    <h1>Resume</h1>
+  )
+}
