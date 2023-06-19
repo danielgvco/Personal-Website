1775e18116ed99bfbf8182d7e63ca121195d22c7
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-19 01:19:15 -0500

    Add a test for mobile navbar so for deployment test at vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import { SheetSize } from './components/NavbarMobile'
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <h1>
+      Home
+      <SheetSize />
+    </h1>
   )
 }
