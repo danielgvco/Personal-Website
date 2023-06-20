0b159de3b4b0dc28562c6521390a5d6ce2f698e9
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-19 20:47:56 -0500

    Better MobileNavbar layout and fixed the closing of the navbar when pressing the buttons projects and contact me

--- app/components/NavbarMobile.tsx
+++ app/components/NavbarMobile.tsx
@@ -1,9 +1,9 @@
 "use client"
-
+import Link from "next/link"
 import ButtonRoute from "./ButtonRoute"
 import ButtonIcon from "./ButtonIcon"
 import DarkModeToggle from "./DarkModeToggle"
-import { Menu } from 'lucide-react'
+import { Menu, Code2, Mail } from 'lucide-react'
 import { Button } from "./ui/button"
 import {
     Sheet,
@@ -27,20 +27,19 @@ export function SheetSize() {
                     <SheetHeader>
                         <SheetTitle>DANIEL VARGAS | PORTFOLIO</SheetTitle>
                     </SheetHeader>
-                    <ul className="grid gap-4 py-4 text-center mt-6">
+                    <ul className="grid gap-4 py-4 text-center mt-8">
                         <li><SheetClose asChild><ButtonRoute route="/" label="HOME" /></SheetClose></li>
                         <li><SheetClose asChild><ButtonRoute route="/about" label="ABOUT" /></SheetClose></li>
-                        <li><SheetClose asChild><ButtonRoute route="/projects" label="PROJECTS" /></SheetClose></li>
                         <li><SheetClose asChild><ButtonRoute route="/resume" label="RESUME" /></SheetClose></li>
                     </ul>
-                    <div className="flex flex-col space-y-4 mt-72">
+                    <div className="flex flex-col space-y-4 mt-56">
                         <ul className="flex justify-between">
                             <li className="w-1/4"><ButtonIcon iconName="linkedin" route="https://www.linkedin.com/in/danielgvco/" style="w-full" variant="outline"/></li>
                             <li className="w-1/4"><ButtonIcon iconName="github" route="https://github.com/danielgvco" style="w-full" variant="outline"/></li>
                             <li className="w-1/4"><DarkModeToggle/></li>
                         </ul>
-                        <SheetClose asChild><ButtonIcon iconName="code" label="PROJECTS" route="/projects" variant="default" target="" rel="" /></SheetClose>
-                        <SheetClose asChild><ButtonIcon iconName="mail" label="CONTACT ME" route="/contact" variant="outline" target="" rel="" /></SheetClose>
+                        <SheetClose asChild><Button asChild><Link href="/projects"><Code2 className="h-full aspect-square"/><span className="ml-2">PROJECTS</span></Link></Button></SheetClose>
+                        <SheetClose asChild><Button asChild variant="outline"><Link href="/contact"><Mail className="h-full aspect-square"/><span className="ml-2">CONTACT ME</span></Link></Button></SheetClose>
                     </div>
                 </SheetContent>
             </Sheet>
