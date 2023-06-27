354dcee225ca2c7d02b3bc925f94b9410a6cca62
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-26 22:07:34 -0500

    Tailwind-animated added, and testing of animation for contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -8,14 +8,14 @@ export default function Contact() {
   return (
     <div className='full_height flex flex-col justify-center'>
       <div className='mx-auto w-3/4 flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
-        <div className='p-4 w-full rounded-xl h-fit shadow-md lg:w-1/3'>
+        <div className='p-4 w-full rounded-xl h-fit shadow-md lg:w-1/3 animate-fade-down animate-once animate-ease-in-out'>
           <h2 className='pl-4 mb-4'>Contact Information</h2>
           <div className='flex'>
             <Button variant="link"><Mail className='mr-2' />work@danielgv.co</Button>
             <CopyToClipboard text='work@danielgv.co'/>
           </div>
         </div>
-        <div className='w-full p-4 rounded-xl shadow-md lg:w-2/3'>
+        <div className='w-full p-4 rounded-xl shadow-md lg:w-2/3 animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
           <h2 className='mb-4'>Contact Form</h2>
           <InputReactHookForm />
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -96,5 +96,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [
+    require("tailwindcss-animate"),
+    require('tailwindcss-animated')
+  ],
 }
