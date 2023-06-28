99bd4562b4fb9d3939bbfa4d73aaf2e7b07d71a1
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-28 13:27:16 -0500

    added alt for each logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,8 +12,8 @@ const Navbar: FC = () => {
     <nav className="flex h-14 max-h-14 items-center justify-between px-4 top-0 shadow">
       <div className="animate-fade-down animate-once animate-delay-0 animate-ease-in-out">
         <Link href="/">
-          <Image src='/DANIEL VARGAS Light.png' alt="logo" width={300} height={45} className="dark:hidden" />
-          <Image src='/DANIEL VARGAS Dark.png' alt="logo" width={300} height={45} className="hidden dark:flex"/>
+          <Image src='/DANIEL VARGAS Light.png' alt="logo-lightmode" width={300} height={45} className="dark:hidden" />
+          <Image src='/DANIEL VARGAS Dark.png' alt="logo-darkmode" width={300} height={45} className="hidden dark:flex"/>
         </Link>
       </div>
       <ul className="hidden lg:flex items-center space-x-2">
