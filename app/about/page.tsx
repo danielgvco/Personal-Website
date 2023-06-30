5d131640b56eff9815d0b443436910ca54e8174b
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-29 23:55:08 -0500

    some layouts added, and commit before the beginning of the resume page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image'
 import { Metadata } from 'next'
+import { Button } from '../components/ui/button'
 
 export const metadata: Metadata = {
   title: 'Daniel | About',
@@ -8,6 +9,15 @@ export const metadata: Metadata = {
 
 export default function About() {
   return (
-    <h1>About</h1>
+    <div className='full_height flex flex-col justify-center'>
+      <div className='mx-auto w-3/4 flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
+        <div className='p-4 w-full rounded-xl h-fit shadow-md lg:w-1/3 animate-fade-down animate-once animate-ease-in-out'>
+          <h2 className='pl-4 mb-4'>Contact Information</h2>
+        </div>
+        <div className='w-full p-4 rounded-xl shadow-md lg:w-2/3 animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
+          <h2 className='mb-4'>Contact Form</h2>
+        </div>
+      </div>
+    </div>
   )
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import { Metadata } from 'next'
+import { Button } from '../components/ui/button'
+import ButtonIcon from '../components/ButtonIcon'
+import ButtonRoute from '../components/ButtonRoute'
 
 export const metadata: Metadata = {
   title: 'Daniel | Resume',
@@ -8,6 +12,21 @@ export const metadata: Metadata = {
 
 export default function Resume() {
   return (
-    <h1>Resume</h1>
+    <div className='full_height flex flex-col justify-center'>
+      <div className='mx-auto w-11/12 flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
+        <div className='p-4 w-full rounded-xl h-fit shadow-md lg:w-1/3 animate-fade-down animate-once animate-ease-in-out'>
+          <ul className='flex flex-row justify-center lg:flex-col'>
+            <li>Daniel Vargas</li>
+            <li className='lg:hidden px-2'>|</li>
+            <li><Link href="/">work@danielgv.co</Link></li>
+            <li className='lg:hidden px-2'>|</li>
+            <li>Vancouver, B.C.</li>
+          </ul>
+        </div>
+        <div className='w-full p-4 rounded-xl shadow-md lg:w-2/3 animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
+          <h2 className='mb-4'>Contact Form</h2>
+        </div>
+      </div>
+    </div>
   )
 }
