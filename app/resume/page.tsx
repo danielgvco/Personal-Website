3ff0f2dd7edb688f2d9d072dd1a89db20c6f68f7
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-06-30 19:02:04 -0500

    gradient test

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -14,8 +14,8 @@ export const metadata: Metadata = {
 export default function Resume() {
   return (
     <div className='full_height flex flex-col justify-center'>
-      <div className='mx-auto w-11/12 flex flex-col space-y-4 lg:flex-row lg:space-x-4 lg:space-y-0'>
-        <div className='p-4 w-full rounded-xl h-fit space-y-4 shadow-md lg:w-1/3 animate-fade-down animate-once animate-ease-in-out'>
+      <div className='mx-auto w-11/12 flex flex-col space-y-4 lg:w-3/4 lg:flex-row lg:space-x-4 lg:space-y-0'>
+        <div className='p-4 w-full rounded-xl h-fit space-y-4 shadow-md lg:w-fit lg:min-w-[15rem] animate-fade-down animate-once animate-ease-in-out'>
           <ul className='flex flex-col'>
             <div className='flex flex-row justify-center lg:flex-col'>
               <li>Daniel Vargas</li>
@@ -24,21 +24,15 @@ export default function Resume() {
             </div>
             <li className='flex justify-center lg:flex-col'>Vancouver, B.C.</li>
           </ul>
-          <ul className='hidden flex-row justify-center space-x-2 lg:flex lg:space-x-0 lg:flex-col'>
-            <li><Button className='w-full'><Download className='mr-2'/>Download PDF</Button></li>
-            <li className="animate-fade-down animate-once animate-delay-[800ms] animate-ease-in-out"><ButtonIcon iconName="mail" route="/contact" variant="outline" target="" rel="" /></li>
-            <li className="animate-fade-down animate-once animate-delay-[500ms] animate-ease-in-out"><ButtonIcon iconName="linkedin" route="https://www.linkedin.com/in/danielgvco/" variant="outline" /></li>
-            <li className="animate-fade-down animate-once animate-delay-[600ms] animate-ease-in-out"><ButtonIcon iconName="github" route="https://github.com/danielgvco" variant="outline" /></li>
-          </ul>
-          <ul className='flex flex-row justify-center space-x-2 lg:hidden lg:space-x-0 lg:flex-col'>
-            <li className="animate-fade-down animate-once animate-delay-[600ms] animate-ease-in-out"><ButtonIcon iconName="github" route="https://github.com/danielgvco" variant="outline" /></li>
-            <li className="animate-fade-down animate-once animate-delay-[500ms] animate-ease-in-out"><ButtonIcon iconName="linkedin" route="https://www.linkedin.com/in/danielgvco/" variant="outline" /></li>
-            <li className="animate-fade-down animate-once animate-delay-[800ms] animate-ease-in-out"><ButtonIcon iconName="mail" route="/contact" variant="outline" target="" rel="" /></li>
-            <li><Button className='w-full'><Download className='mr-2'/>Download PDF</Button></li>
+          <ul className='flex flex-row justify-center space-x-2 lg:space-x-0 lg:flex-col'>
+            <li className="flex lg:hidden"><ButtonIcon iconName="github" route="https://github.com/danielgvco" variant="outline" /></li>
+            <li className="flex lg:hidden"><ButtonIcon iconName="linkedin" route="https://www.linkedin.com/in/danielgvco/" variant="outline" /></li>
+            <li className="flex lg:hidden"><ButtonIcon iconName="mail" route="/contact" variant="outline" target="" rel="" /></li>
+            <li><Button><Download className='mr-2'/>Download PDF</Button></li>
           </ul>
         </div>
-        <div className='w-full p-4 rounded-xl shadow-md lg:w-2/3 animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
-          <h2 className='mb-4'>Contact Form</h2>
+        <div className='w-full p-4 rounded-xl shadow-md animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
+          <h1 className='mb-4'>Resume</h1>
         </div>
       </div>
     </div>
