9070154e57942079859b33c1b79545e13a71a0ac
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-01 01:33:58 -0500

    Resume Page with a little outdated info

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -31,11 +31,12 @@ export default function Resume() {
             <li><Button><Download className='mr-2' />Download PDF</Button></li>
           </ul>
         </div>
-        <div className='w-full p-4 rounded-xl space-y-4 shadow-md animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
+        <div className='w-full p-4 rounded-xl space-y-6 shadow-md animate-fade-down animate-once animate-delay-150 animate-ease-in-out'>
           <div className=''>
             <h2 className='font-700 text-xl border-b border-gray-300'>Skills</h2>
             <ul className='space-y-2 list-disc pl-5'>
               <li><span className='font-700'>Programming Languages:</span> Python, Java, JavaScript, TypeScript, HTML / CSS</li>
+              <li><span className='font-700'>Frameworks and Libraries:</span> NextJS, React, Tailwind CSS</li>
               <li><span className='font-700'>Database Management:</span> MySQL, MongoDB</li>
               <li><span className='font-700'>Tools:</span> Git, Github, Bash Terminal</li>
               <li><span className='font-700'>Concepts:</span> Version Control, Object-Oriented Programming, Data Structures, Algorithms</li>
@@ -44,59 +45,61 @@ export default function Resume() {
           </div>
           <div className=''>
             <h2 className='font-700 text-xl border-b border-gray-300'>Projects</h2>
-            <ol className='list-decimal pl-5'>
+            <ol className='list-decimal pl-5 space-y-2'>
               <li>
                 <span className='font-700'>Personal Portfolio Website</span>
-                <ul className='list-disc pl-5'>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <ul className='list-disc pl-5 space-y-2'>
+                  <li>NextJS | TypeScript | Tailwind CSS project</li>
+                  <li>Learned about version control and how important it is for an organized project</li>
+                  <li>Made me learn better coding practices</li>
                 </ul>
               </li>
               <li>
                 <span className='font-700'>Asuna - Discord Bot AI</span>
-                <ul className='list-disc pl-5'>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <ul className='list-disc pl-5 space-y-2'>
+                  <li>Implemented OpenAI ChatGPT to intelligently moderate server messages.</li>
+                  <li>Improved the efficiency of any discord server by automating the moderating duty.</li>
+                  <li>Used the Natural Language Processing to perform server tasks 24/7.</li>
                 </ul>
               </li>
               <li>
                 <span className='font-700'>Password Manager with Encryption</span>
-                <ul className='list-disc pl-5'>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <ul className='list-disc pl-5 space-y-2'>
+                  <li>Implemented AES-256 and AES-512 to ensure the safe storage of user credentials.</li>
+                  <li>Learned about Encryption methods, how to use them and ways to make secure.</li>
+                  <li>Applied salt to the encryption in order to make every encryption different from each other.</li>
                 </ul>
               </li>
             </ol>
           </div>
           <div className=''>
             <h2 className='font-700 text-xl border-b border-gray-300'>Education</h2>
-            <ul>
+            <ul className='space-y-2'>
               <li>
-                <span>Red Deer Polytechnic</span>
-                <ul className='list-disc pl-5'>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <div><span className='font-700'>Red Deer Polytechnic</span><span> | Software Development</span></div>
+                <span className='text-sm'>January 2022 - December 2023, Vancouver B.C.</span>
+                <ul className='list-disc pl-5 mt-2 space-y-2'>
+                  <li>Develop Software that solves real-world problems.</li>
+                  <li>Design and management of clean databases.</li>
+                  <li>Implemented good coding practices in order to create projects in an efficient way.</li>
                 </ul>
               </li>
               <li>
-                <span>Coursera</span>
-                <ul className='list-disc pl-5'>
-                  <li></li>
-                  <li></li>
-                  <li></li>
+                <div><span className='font-700'>Coursera</span><span> | Meta Back-End Developer, Computer Software Engineering.</span></div>
+                <span className='text-sm'>January 2023 - July 2023</span>
+                <ul className='list-disc pl-5 mt-2 space-y-2'>
+                  <li>Data storage and databases using MySQL, and how to craft sophisticated SQL queries.</li>
+                  <li>Django web framework and how the front-end consumes data from the REST APIs.</li>
+                  <li>Linux commands and Git repositories to implement version control.</li>
                 </ul>
               </li>
             </ul>
           </div>
           <div className=''>
             <h2 className='font-700 text-xl border-b border-gray-300'>Certifications</h2>
-            <ul className='list-disc pl-5'>
-              <li></li>
-              <li></li>
+            <ul className='list-disc pl-5 space-y-2'>
+              <li><span className='font-700'>Programming in Python</span> | Coursera, February 2023.</li>
+              <li><span className='font-700'>Introduction to Back-End Development</span> | Coursera, January 2023.</li>
             </ul>
           </div>
         </div>
