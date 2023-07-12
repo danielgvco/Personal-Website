fc652113456374b4cec4f0bf101d33bc1b7ddbfe
Daniel Vargas <dani@192.168.39.198>
2023-07-12 14:47:51 -0500

    buttons added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -13,13 +13,15 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="full_height flex flex-col justify-center">
-      <div className='flex flex-col items-center'>
+      <div className='mx-auto flex flex-col items-center'>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem]'>SOFTWARE</p>
         <div className='p-2 border-2 border-primary text-center text-[0.8rem] sm:text-[1rem] lg:text-[1.5rem]'><p>LET'S <TypingEffect words={['CREATE.', 'CODE.', 'BUILD.', 'DESIGN.', 'DEVELOP.']}/></p><p>HIGHLY EFFECTIVE SOLUTIONS FOR REAL WORLD PROBLEMS</p></div>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem]'>DEVELOPER</p>
       </div>
-      <ToastSimple />
-      <div></div>
+      <div className='mx-auto flex flex-col w-[75%] h-28 space-y-4 mt-12 lg:h-14 lg:flex-row lg:space-x-4 lg:space-y-0 lg:w-[60%] lg:mt-4'>
+        <ButtonIcon iconName='code' label='PROJECTS' variant='default'/>
+        <ButtonIcon iconName='mail' label='CONTACT' route='/contact' variant='outline'/>
+      </div>
     </div>
   )
 }
