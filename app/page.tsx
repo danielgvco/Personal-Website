50c97dac77702f94362009cb217cc16d50bd309d
Daniel Vargas <dani@192.168.39.198>
2023-07-12 15:10:28 -0500

    finished config on the home page buttons

--- app/page.tsx
+++ app/page.tsx
@@ -19,8 +19,8 @@ export default function Home() {
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem]'>DEVELOPER</p>
       </div>
       <div className='mx-auto flex flex-col w-[75%] h-28 space-y-4 mt-20 lg:h-14 lg:flex-row lg:space-x-4 lg:space-y-0 lg:w-[60%] lg:mt-4'>
-        <ButtonIcon iconName='code' label='PROJECTS' variant='default'/>
-        <ButtonIcon iconName='mail' label='CONTACT' route='/contact' variant='outline'/>
+        <ButtonIcon iconName='code' label='PROJECTS' route='/projects' variant='default' target='' rel=''/>
+        <ButtonIcon iconName='mail' label='CONTACT' route='/contact' variant='outline' target='' rel=''/>
       </div>
     </div>
   )
