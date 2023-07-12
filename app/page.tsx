fd2aca2a8eeffdcd01c808fba498ace39a10b03e
Daniel Vargas <dani@192.168.39.198>
2023-07-12 14:51:59 -0500

    homepage no scroll in mobile

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className="full_height flex flex-col justify-center">
+    <div className="full_height_no_scroll flex flex-col justify-center">
       <div className='mx-auto flex flex-col items-center'>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem]'>SOFTWARE</p>
         <div className='p-2 border-2 border-primary text-center text-[0.8rem] sm:text-[1rem] lg:text-[1.5rem]'><p>LET'S <TypingEffect words={['CREATE.', 'CODE.', 'BUILD.', 'DESIGN.', 'DEVELOP.']}/></p><p>HIGHLY EFFECTIVE SOLUTIONS FOR REAL WORLD PROBLEMS</p></div>
