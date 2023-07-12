e3e4cea1ad7afea6864162842717523e8db99770
Daniel Vargas <dani@192.168.39.198>
2023-07-12 15:06:30 -0500

    test new mt between homepage buttons and text

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
         <div className='p-2 border-2 border-primary text-center text-[0.8rem] sm:text-[1rem] lg:text-[1.5rem]'><p>LET'S <TypingEffect words={['CREATE.', 'CODE.', 'BUILD.', 'DESIGN.', 'DEVELOP.']}/></p><p>HIGHLY EFFECTIVE SOLUTIONS FOR REAL WORLD PROBLEMS</p></div>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem]'>DEVELOPER</p>
       </div>
-      <div className='mx-auto flex flex-col w-[75%] h-28 space-y-4 mt-12 lg:h-14 lg:flex-row lg:space-x-4 lg:space-y-0 lg:w-[60%] lg:mt-4'>
+      <div className='mx-auto flex flex-col w-[75%] h-28 space-y-4 mt-20 lg:h-14 lg:flex-row lg:space-x-4 lg:space-y-0 lg:w-[60%] lg:mt-4'>
         <ButtonIcon iconName='code' label='PROJECTS' variant='default'/>
         <ButtonIcon iconName='mail' label='CONTACT' route='/contact' variant='outline'/>
       </div>
