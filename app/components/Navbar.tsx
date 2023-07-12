14a581c65643de4432bffc5ab8d152da571dc441
Daniel Vargas <dani@192.168.39.198>
2023-07-12 16:51:27 -0500

    Capitalized the menu buttons from the navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,16 +17,16 @@ const Navbar: FC = () => {
         </Link>
       </div>
       <ul className="hidden lg:flex items-center space-x-2">
-        <li className="animate-fade-down animate-once animate-delay-[100ms] animate-ease-in-out"><ButtonRoute label="Home" route="/" variant="ghost" /></li>
-        <li className="animate-fade-down animate-once animate-delay-[200ms] animate-ease-in-out"><ButtonRoute label="About" route="/about" variant="ghost" /></li>
-        <li className="animate-fade-down animate-once animate-delay-[300ms] animate-ease-in-out"><ButtonRoute label="Projects" route="/projects" variant="ghost" /></li>
-        <li className="animate-fade-down animate-once animate-delay-[400ms] animate-ease-in-out"><ButtonRoute label="Resume" route="/resume" variant="ghost" /></li>
+        <li className="animate-fade-down animate-once animate-delay-[100ms] animate-ease-in-out"><ButtonRoute label="HOME" route="/" variant="ghost" /></li>
+        <li className="animate-fade-down animate-once animate-delay-[200ms] animate-ease-in-out"><ButtonRoute label="ABOUT" route="/about" variant="ghost" /></li>
+        <li className="animate-fade-down animate-once animate-delay-[300ms] animate-ease-in-out"><ButtonRoute label="PROJECTS" route="/projects" variant="ghost" /></li>
+        <li className="animate-fade-down animate-once animate-delay-[400ms] animate-ease-in-out"><ButtonRoute label="RESUME" route="/resume" variant="ghost" /></li>
       </ul>
       <ul className="hidden lg:flex items-center space-x-2 max-h-11">
         <li className="animate-fade-down animate-once animate-delay-[500ms] animate-ease-in-out"><ButtonIcon iconName="linkedin" route="https://www.linkedin.com/in/danielgvco/" variant="outline" /></li>
         <li className="animate-fade-down animate-once animate-delay-[600ms] animate-ease-in-out"><ButtonIcon iconName="github" route="https://github.com/danielgvco" variant="outline" /></li>
         <li className="animate-fade-down animate-once animate-delay-[700ms] animate-ease-in-out"><DarkModeToggle /></li>
-        <li className="animate-fade-down animate-once animate-delay-[800ms] animate-ease-in-out"><ButtonIcon iconName="mail" label="Contact Me" route="/contact" variant="default" target="" rel="" style="max-h-11" /></li>
+        <li className="animate-fade-down animate-once animate-delay-[800ms] animate-ease-in-out"><ButtonIcon iconName="mail" label="CONTACT ME" route="/contact" variant="default" target="" rel="" style="max-h-11" /></li>
       </ul>
       <ul className="lg:hidden flex items-center space-x-2">
         <li className="animate-fade-down animate-once animate-delay-[100ms] animate-ease-in-out"><DarkModeToggle /></li>
