c0dc0d2be3d0ddf7b42b0552180926c91fa91ebc
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-14 09:48:34 -0500

    changed let's to i like to text in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
     <div className="full_height_no_scroll flex flex-col justify-center">
       <div className='mx-auto flex flex-col items-center'>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem] animate-fade-down animate-once animate-delay-[100ms] animate-ease-in-out'>SOFTWARE</p>
-        <div className='p-2 border-2 border-primary text-center text-[0.8rem] sm:text-[1rem] lg:text-[1.5rem] animate-fade-down animate-once animate-delay-[200ms] animate-ease-in-out'><p>LET'S <TypingEffect words={['CREATE.', 'CODE.', 'BUILD.', 'DESIGN.', 'DEVELOP.']}/></p><p>HIGHLY EFFECTIVE SOLUTIONS FOR REAL WORLD PROBLEMS</p></div>
+        <div className='p-2 border-2 border-primary text-center text-[0.8rem] sm:text-[1rem] lg:text-[1.5rem] animate-fade-down animate-once animate-delay-[200ms] animate-ease-in-out'><p>I LIKE TO <TypingEffect words={['CREATE.', 'CODE.', 'BUILD.', 'DESIGN.', 'DEVELOP.']}/></p><p>HIGHLY EFFECTIVE SOLUTIONS FOR REAL WORLD PROBLEMS</p></div>
         <p className='font-700 text-[4rem] sm:text-[6rem] lg:text-[8rem] animate-fade-down animate-once animate-delay-[300ms] animate-ease-in-out'>DEVELOPER</p>
       </div>
       <div className='mx-auto flex flex-col w-[75%] h-28 space-y-4 mt-20 lg:h-14 lg:flex-row lg:space-x-4 lg:space-y-0 lg:w-[60%] lg:mt-4 animate-fade-down animate-once animate-delay-[400ms] animate-ease-in-out'>
