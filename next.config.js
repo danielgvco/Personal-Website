55e4385b74df3a42d6598a21cbbc9703dc913762
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-14 08:55:04 -0500

    Removed anything related to next mdx

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    experimental: {
-        mdxRs: true,
-    },
-}
+const nextConfig = {}
 
-const withMDX = require('@next/mdx')()
-module.exports = withMDX(nextConfig)
+module.exports = nextConfig
