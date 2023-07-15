0d53d7351d1a86aa29ff06d0cea4bc8797763684
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-15 11:54:48 -0500

    copy env script added, fixed path of the fix script

--- postbuild-fix/scripts/contentlayer-fix.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Replace the corrupted lines with the fixed ones
-sed -i '7c\import pkg from '\''@opentelemetry/sdk-trace-base'\'';\nconst { ConsoleSpanExporter, SimpleSpanProcessor } = pkg;' ./node_modules/@effect-ts/otel/_mjs/Processor/Simple/index.mjs
-sed -i '10c\import pkg from '\''@opentelemetry/sdk-trace-node'\'';\nconst { NodeTracerProvider } = pkg;' ./node_modules/@effect-ts/otel-sdk-trace-node/_mjs/index.mjs
-sed -i '5,6c\import resourcePkg from '\''@opentelemetry/resources'\'';\nconst { Resource } = resourcePkg;\nimport semanticConventionsPkg from '\''@opentelemetry/semantic-conventions'\'';\nconst { SemanticResourceAttributes } = semanticConventionsPkg;' ./node_modules/@contentlayer/utils/dist/tracing-effect/index.js
---
+++ postbuild/scripts/copy_env.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Copy .env.example to .env.local
+cp .env.example .env.local
