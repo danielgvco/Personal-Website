a064758f081ad9c6bd82b0cf883e165cd3450c09
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-15 12:13:37 -0500

    Script structure small fix and added preinstall to see if works on vercel

--- postbuild/scripts/contentlayer-fix.sh
+++ postbuild/scripts/contentlayer-fix.sh
@@ -3,15 +3,18 @@
 # Replace the corrupted lines with the fixed ones
 sed -i '' '7c\
 import pkg from '\''@opentelemetry/sdk-trace-base'\'';\
-const { ConsoleSpanExporter, SimpleSpanProcessor } = pkg;' ./node_modules/@effect-ts/otel/_mjs/Processor/Simple/index.mjs
+const { ConsoleSpanExporter, SimpleSpanProcessor } = pkg;
+' ./node_modules/@effect-ts/otel/_mjs/Processor/Simple/index.mjs
 
 sed -i '' '10c\
 import pkg from '\''@opentelemetry/sdk-trace-node'\'';\
-const { NodeTracerProvider } = pkg;' ./node_modules/@effect-ts/otel-sdk-trace-node/_mjs/index.mjs
+const { NodeTracerProvider } = pkg;
+' ./node_modules/@effect-ts/otel-sdk-trace-node/_mjs/index.mjs
 
 sed -i '' '5,6c\
 import resourcePkg from '\''@opentelemetry/resources'\'';\
 const { Resource } = resourcePkg;\
 import semanticConventionsPkg from '\''@opentelemetry/semantic-conventions'\'';\
-const { SemanticResourceAttributes } = semanticConventionsPkg;' ./node_modules/@contentlayer/utils/dist/tracing-effect/index.js
+const { SemanticResourceAttributes } = semanticConventionsPkg;
+' ./node_modules/@contentlayer/utils/dist/tracing-effect/index.js
 
