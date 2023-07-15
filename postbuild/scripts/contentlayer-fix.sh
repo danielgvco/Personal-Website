2898a5d79d23aace569f39492108072ef41df626
Daniel Vargas <dani@Daniels-MacBook-Air.local>
2023-07-15 15:07:54 -0500

    changed sed bash command structure

--- postbuild/scripts/contentlayer-fix.sh
+++ postbuild/scripts/contentlayer-fix.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Replace the corrupted lines with the fixed ones
-sed -i '' '7c\
+sed '-i' '7c\
 import pkg from '\''@opentelemetry/sdk-trace-base'\'';\
 const { ConsoleSpanExporter, SimpleSpanProcessor } = pkg;\
 ' node_modules/@effect-ts/otel/_mjs/Processor/Simple/index.mjs
 
-sed -i '' '10c\
+sed '-i' '10c\
 import pkg from '\''@opentelemetry/sdk-trace-node'\'';\
 const { NodeTracerProvider } = pkg;\
 ' node_modules/@effect-ts/otel-sdk-trace-node/_mjs/index.mjs
 
-sed -i '' '5,6c\
+sed '-i' '5,6c\
 import resourcePkg from '\''@opentelemetry/resources'\'';\
 const { Resource } = resourcePkg;\
 import semanticConventionsPkg from '\''@opentelemetry/semantic-conventions'\'';\
